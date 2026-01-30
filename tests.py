b391bddd61dc20810287ef5e8ef21a6736faddb2
anagy66 <a.nagy66@yahoo.ca>
2026-01-29 17:42:43 -0700

    third commit

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import app
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         import app
-        self.assertEqual(app.hello_world(), 'Hello, World!')
+        self.assertEqual(app.hello_world(), 'Hello again, World!')
 
 if __name__ == '__main__':
     unittest.main()
